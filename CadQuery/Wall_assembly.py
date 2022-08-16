6c7a07f0e0df2d786cc1dd8ba179c7518f0405cc
Technicus <felixgardner@gmail.com>
2022-08-15 21:45:45 -0500

    Assembly Development

--- CadQuery/Wall_assembly.py
+++ CadQuery/Wall_assembly.py
@@ -3,6 +3,7 @@
 
 import cadquery as cq
 
+# Set board dimentions for dimentional lumbar in metric
 board_dimensions_2x4 = {
     "width": 38,
     "depth": 89,
@@ -15,11 +16,17 @@ board_dimensions_2x6 = {
     "radius": 3.2
 }
 
-wall_dimensions = {
-    "stud": 2908.3,
-    "bottom_plate": 2438
+# Set wall dimentions, material lengths, number of studs.
+# todo: include parameters for openings, corners and additional features.
+wall_parameters = {
+    "stud_length": 2908.3, # 114.5"
+    "bottom_plate_length": 2438, # 8'
+    "top_plate_length": 2438, # 8'
+    "stud_distribution": 406.4, # 16"
+    "stud_count": 6 # bottom_plate_length / stud_distribution round up
 }
 
+# Create board sketch profiles.
 board_profile_2x4 = (
     cq.Sketch()
     .rect(board_dimensions_2x4["width"], board_dimensions_2x4["depth"])
@@ -34,30 +41,63 @@ board_profile_2x6 = (
     .fillet(board_dimensions_2x6["radius"])
 )
 
+# Create the boards and match them to length for wall.
 board_wall_stud = (
     cq.Workplane()
     .placeSketch(board_profile_2x4)
-    .extrude(wall_dimensions["stud"])
+    .extrude(wall_parameters["stud_length"])
 )
-
-# tag mating edges
-board_wall_stud.faces("<Z").tag("stud_bottom")
 
 board_bottom_plate = (
     cq.Workplane()
     .placeSketch(board_profile_2x6)
-    .extrude(wall_dimensions["bottom_plate"])
+    .extrude(wall_parameters["bottom_plate_length"])
 )
 
-# tag mating edges
+# Tag mating edges
+board_wall_stud.faces("<Z").tag("stud_bottom")
 board_bottom_plate.faces(">X").tag("bottom_plate_top")
 
+# Create the wall assembly.
+wall = (
+    cq.Assembly()
+)
+
+#wall.add(
+    #board_bottom_plate,
+    #loc = cq.Location(cq.Vector(0, 0, 0), cq.Vector(1, 0, 0), 90),
+    #name = "bottom_plate",
+    #color= cq.Color("green")
+    #)
+
+#wall.add(
+    #board_wall_stud,
+    ##loc = cq.Location(cq.Vector(0, 0, 0), cq.Vector(1, 0, 0), 180),
+    #name = "stud",
+    #color=cq.Color("brown")
+    #)
+
+board_bottom_plate = board_bottom_plate.rotate((0,0,0), (0,1,0), 90)
 
 wall = (
         cq.Assembly()
+        .add(board_bottom_plate, name="board_bottom_plate", color=cq.Color("green"))
+        .add(board_wall_stud, name="stud0", color=cq.Color("brown"))
+        .add(board_wall_stud, name="stud1", color=cq.Color("brown"))
+        .constrain("board_bottom_plate", "Fixed")
+        .constrain("stud0", "FixedRotation", (0,0,0))
+        .constrain("stud1", "FixedRotation", (0,0,0))
+
+        .constrain("stud0?stud_bottom", "board_bottom_plate@faces@>Z", "PointInPlane")
+        .constrain("stud1?stud_bottom", "board_bottom_plate@faces@>Z", "PointInPlane")
+
+        .constrain("board_bottom_plate@faces@<X", "stud0@faces@<X", "PointInPlane", 0)
+        .constrain("board_bottom_plate@faces@<X", "stud1@faces@<X", "PointInPlane", 380)
+
+        .constrain("board_bottom_plate@faces@>Y", "stud0@faces@>Y", "PointInPlane", 0)
+        .constrain("board_bottom_plate@faces@>Y", "stud1@faces@>Y", "PointInPlane", 0)
 )
 
+wall.solve()
 
-
-show_object(board_wall_stud, options = dict(alpha = 0.0, color = ("brown")))
-show_object(board_bottom_plate, options = dict(alpha = 0.0, color = ("green")))
+show_object(wall)
